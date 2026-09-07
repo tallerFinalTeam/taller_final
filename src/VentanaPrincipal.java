047d4998818dc16dea17dbfba0a3330b458e70d4
wilmar martinez <martinezwilmar313@gmail.com>
2026-09-07 14:55:59 -0500

    se hizo el formulario

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-public class VentanaPrincipal extends JFrame{
+
+public class VentanaPrincipal extends JFrame {
+
+    private JTextField campoTitulo, campoAutor, campoIsbn, campoGenero, campoAnio, campoCopias;
 
     public VentanaPrincipal() {
         setTitle("Sistema de gestion de biblioteca");
@@ -8,11 +11,39 @@ public class VentanaPrincipal extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        JPanel panelFormulario = new JPanel();
+        panelFormulario.setLayout(new GridLayout(6, 2, 5, 5));
+
+        panelFormulario.add(new JLabel("Título:"));
+        campoTitulo = new JTextField();
+        panelFormulario.add(campoTitulo);
+
+        panelFormulario.add(new JLabel("Autor:"));
+        campoAutor = new JTextField();
+        panelFormulario.add(campoAutor);
+
+        panelFormulario.add(new JLabel("ISBN/Código:"));
+        campoIsbn = new JTextField();
+        panelFormulario.add(campoIsbn);
+
+        panelFormulario.add(new JLabel("Género:"));
+        campoGenero = new JTextField();
+        panelFormulario.add(campoGenero);
+
+        panelFormulario.add(new JLabel("Año:"));
+        campoAnio = new JTextField();
+        panelFormulario.add(campoAnio);
+
+        panelFormulario.add(new JLabel("Copias disponibles:"));
+        campoCopias = new JTextField();
+        panelFormulario.add(campoCopias);
+
+        add(panelFormulario, BorderLayout.NORTH);
+
         setVisible(true);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new VentanaPrincipal();
     }
 }
-
