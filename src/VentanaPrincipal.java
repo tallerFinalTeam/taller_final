5fd27a76b21f5fa1e28fbb3ce4df527eb37c3834
wilmar martinez <martinezwilmar313@gmail.com>
2026-09-16 09:40:22 -0500

    ultimos detalles

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -96,3 +96,109 @@ public class VentanaPrincipal extends JFrame {
         String[] columnas = {"Titulo", "Autor", "ISBN", "Genero", "Año", "Copias"};
         modeloTabla = new DefaultTableModel(columnas, 0);
         tablaLibros = new JTable(modeloTabla);
+
+        JScrollPane scroll = new JScrollPane(tablaLibros);
+        add(scroll, BorderLayout.CENTER);
+    }
+
+    public void agregarLibro() {
+        String titulo = campoTitulo.getText();
+        String autor = campoAutor.getText();
+        String genero = comboGenero.getSelectedItem().toString();
+        String textoIsbn = campoIsbn.getText();
+        String textoAnio = campoAnio.getText();
+        String textoCopias = campoCopias.getText();
+
+        if (titulo.equals("") || autor.equals("") ||
+                textoIsbn.equals("") || textoAnio.equals("") || textoCopias.equals("")) {
+            JOptionPane.showMessageDialog(this, "Todos los campos son obligatorios");
+            return;
+        }
+
+        int codigo = Integer.parseInt(textoIsbn);
+        int anio = Integer.parseInt(textoAnio);
+        int copias = Integer.parseInt(textoCopias);
+
+        int anioActual = java.time.Year.now().getValue();
+
+        if (anio > anioActual) {
+            JOptionPane.showMessageDialog(this, "El año no puede ser mayor al año actual");
+            return;
+        }
+
+        if (copias < 0) {
+            JOptionPane.showMessageDialog(this, "Las copias no pueden ser negativas");
+            return;
+        }
+
+        libro nuevoLibro = new libro(titulo, autor, genero, codigo, anio, copias);
+
+        boolean seAgrego = biblioteca.agregarLibro(nuevoLibro);
+
+        if (seAgrego == false) {
+            JOptionPane.showMessageDialog(this, "Ya existe un libro con ese codigo");
+        } else {
+            mostrarTodos();
+            limpiarCampos();
+        }
+    }
+
+    public void mostrarTodos() {
+        modeloTabla.setRowCount(0);
+
+        for (libro l : biblioteca.obtenerTodos()) {
+            modeloTabla.addRow(new Object[]{
+                    l.getTitulo(),
+                    l.getAutor(),
+                    l.getCodigo(),
+                    l.getCategoriaLibro(),
+                    l.getAnoPublicacion(),
+                    l.getCopiasDisponibles()
+            });
+        }
+    }
+
+    public void filtrarPorAutor() {
+        String autor = campoAutor.getText();
+
+        modeloTabla.setRowCount(0);
+
+        for (libro l : biblioteca.filtrarPorAutor(autor)) {
+            modeloTabla.addRow(new Object[]{
+                    l.getTitulo(),
+                    l.getAutor(),
+                    l.getCodigo(),
+                    l.getCategoriaLibro(),
+                    l.getAnoPublicacion(),
+                    l.getCopiasDisponibles()
+            });
+        }
+    }
+
+    public void eliminarLibro() {
+        int filaSeleccionada = tablaLibros.getSelectedRow();
+
+        if (filaSeleccionada == -1) {
+            JOptionPane.showMessageDialog(this, "Selecciona un libro primero");
+            return;
+        }
+
+        int codigo = Integer.parseInt(modeloTabla.getValueAt(filaSeleccionada, 2).toString());
+
+        int respuesta = JOptionPane.showConfirmDialog(this, "Seguro que quieres eliminar este libro?");
+
+        if (respuesta == JOptionPane.YES_OPTION) {
+            biblioteca.eliminarLibro(codigo);
+            mostrarTodos();
+        }
+    }
+
+    public void limpiarCampos() {
+        campoTitulo.setText("");
+        campoAutor.setText("");
+        campoIsbn.setText("");
+        comboGenero.setSelectedIndex(0);
+        campoAnio.setText("");
+        campoCopias.setText("");
+    }
+}
